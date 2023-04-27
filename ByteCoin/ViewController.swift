d9b37768f2c231b28d98f43000e50a92077f78e6
Evgenii Zhitnikov <124378718+ek-zhitnikov@users.noreply.github.com>
2023-04-27 17:53:29 +0400

    Initial Commit

---
+++ ByteCoin/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ByteCoin
+//
+//  Created by Евгений Житников on 27.04.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
